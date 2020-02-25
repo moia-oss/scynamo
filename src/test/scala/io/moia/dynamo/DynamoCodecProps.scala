0d26e707f789d75112f06c4e3322f4549a3acb6b
Markus Hauck <markus1189@gmail.com>
2020-02-25 08:46:17 +0100

    Use propertyWithSeed

--- src/test/scala/io/moia/dynamo/DynamoCodecProps.scala
+++ src/test/scala/io/moia/dynamo/DynamoCodecProps.scala
@@ -6,25 +6,27 @@ import org.scalacheck.{Gen, Prop, Properties}
 import org.scalacheck.Prop.propBoolean
 
 class DynamoCodecProps extends Properties("DynamoCodec") {
-  property("decode .encode === id (int)") = Prop.forAll { value: Int => decodeAfterEncodeIsIdentity(value) }
+  propertyWithSeed("decode .encode === id (int)", None) = Prop.forAll { value: Int => decodeAfterEncodeIsIdentity(value) }
 
-  property("decode .encode === id (long)") = Prop.forAll { value: Long => decodeAfterEncodeIsIdentity(value) }
+  propertyWithSeed("decode .encode === id (long)", None) = Prop.forAll { value: Long => decodeAfterEncodeIsIdentity(value) }
 
-  property("decode .encode === id (float)") = Prop.forAll { value: Float => decodeAfterEncodeIsIdentity(value) }
+  propertyWithSeed("decode .encode === id (float)", None) = Prop.forAll { value: Float => decodeAfterEncodeIsIdentity(value) }
 
-  property("decode .encode === id (double)") = Prop.forAll { value: Double => decodeAfterEncodeIsIdentity(value) }
+  propertyWithSeed("decode .encode === id (double)", None) = Prop.forAll { value: Double => decodeAfterEncodeIsIdentity(value) }
 
-  property("decode .encode === id (string)") = Prop.forAll { value: String => decodeAfterEncodeIsIdentity(value) }
+  propertyWithSeed("decode .encode === id (string)", None) = Prop.forAll { value: String => decodeAfterEncodeIsIdentity(value) }
 
-  property("decode .encode === id (boolean)") = Prop.forAll { value: Boolean => decodeAfterEncodeIsIdentity(value) }
+  propertyWithSeed("decode .encode === id (boolean)", None) = Prop.forAll { value: Boolean => decodeAfterEncodeIsIdentity(value) }
 
-  property("decode .encode === id (instant)") = Prop.forAll(Gen.calendar.map(_.toInstant)) { value: Instant =>
+  propertyWithSeed("decode .encode === id (instant)", None) = Prop.forAll(Gen.calendar.map(_.toInstant)) { value: Instant =>
     decodeAfterEncodeIsIdentity(value)
   }
 
-  property("decode .encode === id (seq)") = Prop.forAll { value: Seq[Int] => decodeAfterEncodeIsIdentity(value) }
+  propertyWithSeed("decode .encode === id (seq)", None) = Prop.forAll { value: Seq[Int] => decodeAfterEncodeIsIdentity(value) }
 
-  property("decode . encode === id (case class)") = Prop.forAll { value: Int => decodeAfterEncodeIsIdentity(DynamoCodecProps.Foo(value)) }
+  propertyWithSeed("decode . encode === id (case class)", None) = Prop.forAll { value: Int =>
+    decodeAfterEncodeIsIdentity(DynamoCodecProps.Foo(value))
+  }
 
   private[this] def decodeAfterEncodeIsIdentity[A: DynamoCodec](value: A): Prop = {
     val codec = DynamoCodec[A]
