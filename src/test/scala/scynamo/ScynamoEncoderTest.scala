f3d9838725e106b6f733e81a5cd6f17754b43f4b
Markus Hauck <markus1189@gmail.com>
2020-03-02 11:18:33 +0100

    Add test that demonstrates auto derivation override functionality

--- src/test/scala/scynamo/ScynamoEncoderTest.scala
+++ src/test/scala/scynamo/ScynamoEncoderTest.scala
@@ -1,16 +1,22 @@
 package scynamo
 
 import cats.data.EitherNec
-import scynamo.ScynamoEncoderTest.Foo
+import scynamo.ScynamoEncoderTest.{Foo, Foo2}
 import scynamo.ScynamoType.ScynamoString
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue
 import scynamo.generic.auto._
 
 class ScynamoEncoderTest extends UnitTest {
   "ScynamoEncoder" should {
-    "support custom overrides" in {
+    "support custom overrides using semiauto derivation" in {
       val result = ObjectScynamoCodec[Foo].encode(Foo(42))
       result.m.get("i").s should ===(s"${Foo.prefix}42")
+    }
+
+    "NOT support custom overrides using auto derivation" in {
+      import scynamo.generic.auto._
+      val result = ObjectScynamoCodec[Foo2].encode(Foo2(42))
+      result.m.get("i").n should ===(s"42")
     }
 
     "auto derivation does not kick-in for Option" in {
@@ -46,5 +52,19 @@ object ScynamoEncoderTest {
     }
 
     implicit val fooCodec: ObjectScynamoCodec[Foo] = scynamo.generic.semiauto.deriveDynamoCodec[Foo]
+  }
+
+  case class Foo2(i: Int)
+
+  object Foo2 {
+    import scynamo.attributevalue.dsl._
+    val prefix = "this-is-a-"
+    implicit val customIntCodec: ScynamoCodec[Int] = new ScynamoCodec[Int] {
+      override def encode(value: Int): AttributeValue =
+        AttributeValue.builder().s(s"$prefix$value").build()
+
+      override def decode(attributeValue: AttributeValue): EitherNec[ScynamoDecodeError, Int] =
+        accessOrTypeMismatch(attributeValue, ScynamoString)(_.sOpt).map(_.stripPrefix(prefix).toInt)
+    }
   }
 }
