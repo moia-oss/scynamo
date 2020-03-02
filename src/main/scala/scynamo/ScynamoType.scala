3ab41dab4c0303cdab4fb61b870ba01bb8fb5f69
Markus Hauck <markus1189@gmail.com>
2020-03-02 10:38:57 +0100

    Use case objects to have toString

--- src/main/scala/scynamo/ScynamoType.scala
+++ src/main/scala/scynamo/ScynamoType.scala
@@ -7,14 +7,14 @@ object TypeTest extends App
 object ScynamoType {
   val MAGIC_TYPE_ATTRIBUTE_NAME = "_MAGIC_TYPE_TAG_"
 
-  object ScynamoNull      extends ScynamoType
-  object ScynamoBool      extends ScynamoType
-  object ScynamoString    extends ScynamoType
-  object ScynamoNumber    extends ScynamoType
-  object ScynamoBinary    extends ScynamoType
-  object ScynamoMap       extends ScynamoType
-  object ScynamoList      extends ScynamoType
-  object ScynamoStringSet extends ScynamoType
-  object ScynamoNumberSet extends ScynamoType
-  object ScynamoBinarySet extends ScynamoType
+  case object ScynamoNull      extends ScynamoType
+  case object ScynamoBool      extends ScynamoType
+  case object ScynamoString    extends ScynamoType
+  case object ScynamoNumber    extends ScynamoType
+  case object ScynamoBinary    extends ScynamoType
+  case object ScynamoMap       extends ScynamoType
+  case object ScynamoList      extends ScynamoType
+  case object ScynamoStringSet extends ScynamoType
+  case object ScynamoNumberSet extends ScynamoType
+  case object ScynamoBinarySet extends ScynamoType
 }
