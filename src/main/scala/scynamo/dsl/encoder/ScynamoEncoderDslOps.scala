68a9e7366bf6d34000873b20252f8fbcfb9d28f3
Markus Hauck <markus1189@gmail.com>
2020-03-05 10:44:50 +0100

    Add Scynamo object to decode responses

--- src/main/scala/scynamo/dsl/encoder/ScynamoEncoderDslOps.scala
+++ src/main/scala/scynamo/dsl/encoder/ScynamoEncoderDslOps.scala
@@ -1,10 +1,13 @@
 package scynamo.dsl.encoder
 
-import scynamo.ScynamoEncoder
+import java.util
+
+import scynamo.{ObjectScynamoEncoder, ScynamoEncoder}
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue
 
 class ScynamoEncoderDslOps[A](val value: A) extends AnyVal {
-  def toAttributeValue(implicit encoder: ScynamoEncoder[A]): AttributeValue = encoder.encode(value)
+  def toAttributeValue(implicit encoder: ScynamoEncoder[A]): AttributeValue                            = encoder.encode(value)
+  def toAttributeValueMap(implicit encoder: ObjectScynamoEncoder[A]): util.Map[String, AttributeValue] = encoder.encodeMap(value)
 }
 
 trait ScynamoEncoderDsl {
