be8e300101d44a2c2ffdab4e320cd487e3e988d1
Markus Hauck <markus1189@gmail.com>
2020-03-25 15:09:47 +0100

    Add unsafe encodings

--- src/main/scala/scynamo/syntax/encoder/ScynamoEncoderDslOps.scala
+++ src/main/scala/scynamo/syntax/encoder/ScynamoEncoderDslOps.scala
@@ -9,8 +9,18 @@ import software.amazon.awssdk.services.dynamodb.model.AttributeValue
 class ScynamoEncoderDslOps[A](val value: A) extends AnyVal {
   def encoded(implicit encoder: ScynamoEncoder[A]): EitherNec[ScynamoEncodeError, AttributeValue] = encoder.encode(value)
 
+  def encodedUnsafe(implicit encoder: ScynamoEncoder[A]): AttributeValue = unsafeUnwrap(encoder.encode(value))
+
   def encodedMap(implicit encoder: ObjectScynamoEncoder[A]): EitherNec[ScynamoEncodeError, util.Map[String, AttributeValue]] =
     encoder.encodeMap(value)
+
+  def encodedMapUnsafe(implicit encoder: ObjectScynamoEncoder[A]): util.Map[String, AttributeValue] =
+    unsafeUnwrap(encoder.encodeMap(value))
+
+  private[this] def unsafeUnwrap[B](input: EitherNec[ScynamoEncodeError, B]): B = input match {
+    case Left(errors) => throw new IllegalArgumentException(s"Decoding as a map failed with: ${errors.map(_.show)}")
+    case Right(value) => value
+  }
 }
 
 trait ScynamoEncoderDsl {
