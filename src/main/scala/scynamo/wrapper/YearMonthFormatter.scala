4ce09fa8460a60ed2ded648b4c6b4603e6466c4d
Bendix Saeltz <bendix@saeltz.de>
2021-07-14 10:30:43 +0200

    Make YearMonthFormatter private

--- src/main/scala/scynamo/wrapper/YearMonthFormatter.scala
+++ src/main/scala/scynamo/wrapper/YearMonthFormatter.scala
@@ -6,6 +6,6 @@ import java.time.format.DateTimeFormatter
   * "Years outside the range 0000 to 9999 must be prefixed by the plus or minus symbol."
   * but the plus symbol is not added by the default `.toString`.
   */
-object YearMonthFormatter {
+private[scynamo] object YearMonthFormatter {
   final val yearMonthFormatter = DateTimeFormatter.ofPattern("uuuu-MM")
 }
